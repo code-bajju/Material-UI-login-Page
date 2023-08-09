8699a173015e35752f2ddce231eccdfc84732054
code-bajju <11212766@mmec.mmdu.org>
2023-08-09 14:53:32 +0530

    Page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Example from './Components/Example/Example';
 import LoginPage from './Components/LoginPage/LoginPage';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Router/Router';
+// import LandingPage from './Components/LandingPage/LandingPage';
+
 
 function App() {
   return (
-    
-    <div className="App">
-     
-      <LoginPage/>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <LoginPage/>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
