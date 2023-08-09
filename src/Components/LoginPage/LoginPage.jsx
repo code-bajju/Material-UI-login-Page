f62b771dd2eb2c05d0374bfbffeacd6f76127b1a
code-bajju <11212766@mmec.mmdu.org>
2023-08-09 14:54:41 +0530

    Skeleton added

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,60 +1,47 @@
 import React from 'react';
 import './LoginPage.css';
-import login from '../../assets/1.png'
 
-function LoginPage() {
-    return (
-        <div className="container d-flex justify-content-center align-items-center min-vh-100">
-            {/* Login Container */}
-            <div className="row border rounded-5 p-3 bg-white shadow box-area">
-                {/* Left Box */}
-                <div className="col-md-6 rounded-4 d-flex justify-content-center align-items-center flex-column left-box" style={{ background: '#103cbe' }}>
-                    <div className="featured-image mb-3">
-                        <img src={login} className="img-fluid" style={{ width: '250px' }} alt="Featured" />
-                    </div>
-                    <p className="text-white fs-2" style={{ fontFamily: 'Courier New, Courier, monospace', fontWeight: 600 }}>Be Verified</p>
-                    <small className="text-white text-wrap text-center" style={{ width: '17rem', fontFamily: 'Courier New, Courier, monospace' }}>Join experienced Designers on this platform.</small>
-                </div>
-
-                {/* Right Box */}
-                <div className="col-md-6 right-box">
-                    <div className="row align-items-center">
-                        <div className="header-text mb-4">
-                            <h2>Hello, Again</h2>
-                            <p>We are happy to have you back.</p>
-                        </div>
-                        <div className="input-group mb-3">
-                            <input type="text" className="form-control form-control-lg bg-light fs-6" placeholder="Email address" />
-                        </div>
-                        <div className="input-group mb-1">
-                            <input type="password" className="form-control form-control-lg bg-light fs-6" placeholder="Password" />
-                        </div>
-                        <div className="input-group mb-5 d-flex justify-content-between">
-                            <div className="form-check">
-                                <input type="checkbox" className="form-check-input" id="formCheck" />
-                                <label htmlFor="formCheck" className="form-check-label text-secondary"><small>Remember Me</small></label>
-                            </div>
-                            <div className="forgot">
-                                <small><a href="#">Forgot Password?</a></small>
-                            </div>
-                        </div>
-                        <div className="input-group mb-3">
-                            <button className="btn btn-lg btn-primary w-100 fs-6">Login</button>
-                        </div>
-                        <div className="input-group mb-3">
-                            <button className="btn btn-lg btn-light w-100 fs-6">
-                                <img src="images/google.png" style={{ width: '20px' }} className="me-2" alt="Google Icon" />
-                                <small>Sign In with Google</small>
-                            </button>
-                        </div>
-                        <div className="row">
-                            <small>Don't have an account? <a href="#">Sign Up</a></small>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
+const LoginPage = () => {
+  return (
+    <div className="v7_60">
+      <div className="image-container">
+        <div className="color-layer"></div>
+        <div className="v8_74"></div>
+      </div>
+      <div className="v8_75"></div>
+      <span className="v8_97">HR Management Platform</span>
+      <span className="v8_98">
+        Manage all HR Operations from the comfort of your home.
+      </span>
+      <div className="v8_99"></div>
+      <div className="name"></div>
+      <div className="v8_76">Welcome</div>
+      <div className="v8_77">Register your account</div>
+      <div className="v8_78">First Name</div>
+      <input type="text" className="v8_87" />
+      <div className="v8_86">Last Name</div>
+      <input type="text" className="v8_95" />
+      <div className="v8_79">E-mail Address</div>
+      <input type="email" className="v8_88" />
+      <div className="v8_80">Phone Number</div>
+      <input type="tel" className="v8_89" />
+      <div className="v8_85">Confirm Password</div>
+      <input
+        type="password"
+        className="v8_94"
+      />
+      <div className="v8_81">Password</div>
+      <input type="password" className="v8_90" />
+      <input
+        type="submit"
+        className="v8_96"
+        value="Create Account"
+      />
+      <div className="v8_83">
+        Already have an account? Log In
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
